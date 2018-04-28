bcd783fd76fec40da176578815b8ea30b9167a92
fankai <cgfankai@qq.com>
2018-04-28 08:10:30 +0800

    新分支，试图增加swagger ui

--- src/main/java/com/crfsdi/ic/sd/vehicleManager/Application.java
+++ src/main/java/com/crfsdi/ic/sd/vehicleManager/Application.java
@@ -3,9 +3,32 @@ package com.crfsdi.ic.sd.vehicleManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.*;
+
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+
+import springfox.documentation.spring.web.plugins.Docket;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public Docket vehicleManagerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
